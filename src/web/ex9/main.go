df71193e74f04267bc2512776d73f64469b67eab
eoeung <eoeung113@gmail.com>
2023-12-15 17:58:50 +0900

    docs ::: 주석 추가

--- src/web/ex9/main.go
+++ src/web/ex9/main.go
@@ -17,8 +17,10 @@ var recvData string
 // 전송
 type SendComponent struct{}
 
+// SendComponent 구조체가 Component 인터페이스를 구현한 것이 됨
 func (self *SendComponent) Operator(data string) {
 	// send data
+	// fmt.Printf("SendComponent.Operator() ::: sentData ::: %v\n", data)
 	sentData = data
 }
 
@@ -27,11 +29,14 @@ type ZipComponent struct {
 	com Component
 }
 
+// ZipComponent 구조체가 Component 인터페이스를 구현한 것이 됨
 func (self *ZipComponent) Operator(data string) {
 	zipData, err := lzw.Write([]byte(data))
 	if err != nil {
 		panic(err)
 	}
+	fmt.Printf("ZipComponent.Operator() ::: zipData ::: %v\n", string(zipData))
+	// fmt.Printf("ZipComponent.Operator() ::: zipData ::: %v\n", zipData)
 	self.com.Operator(string(zipData))
 }
 
@@ -41,14 +46,17 @@ type EncryptComponent struct {
 	com Component
 }
 
-func (self *EncryptComponent) Operator(data string) {
-	encryptData, err := cipher.Encrypt([]byte(data), self.key)
+func (encrypt *EncryptComponent) Operator(data string) {
+	encryptData, err := cipher.Encrypt([]byte(data), encrypt.key)
 	if err != nil {
 		panic(err)
 	}
-	self.com.Operator(string(encryptData))
+	fmt.Printf("EncryptComponent.Operator() ::: encryptData ::: %v\n", string(encryptData))
+	// fmt.Printf("EncryptComponent.Operator() ::: encryptData ::: %v\n", encryptData)
+	encrypt.com.Operator(string(encryptData))
 }
 
+// ##################################
 // 복호화
 type DecryptComponent struct {
 	key string
@@ -86,17 +94,20 @@ func (self *ReadComponent) Operator(data string) {
 func main() {
 	// 암호화하는 component가 압축 component를 가지고 있고,
 	// 압축 component는 전송 component를 가지고 있음
-	sender := &EncryptComponent{
+	sender := &EncryptComponent{ // 구조체 포인터 생성
 		key: "abcde",
 		com: &ZipComponent{
 			com: &SendComponent{},
+			// Component인터페이스 타입인 com 필드가 있는데, SendComponent는 Component인터페이스를 구현함
 		},
 	}
 
 	sender.Operator("Hello World")
-	fmt.Println(sentData) // 이상한 문자가 나온다.
+	fmt.Printf("main() ::: sentData ::: %v\n", sentData) // 이상한 문자가 나온다.
 	// "Hello World"를 암호화하고 압축했기 때문
 
+	fmt.Println()
+	// ######################################
 	receiver := &UnzipComponent{
 		com: &DecryptComponent{
 			key: "abcde",
@@ -104,5 +115,5 @@ func main() {
 		},
 	}
 	receiver.Operator(sentData)
-	fmt.Println(recvData) // Hello World
+	fmt.Printf("main() ::: recvData ::: %v\n", recvData) // Hello World
 }
