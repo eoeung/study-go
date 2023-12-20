3ef1afa03c199ce8f6664cfaf8afa7bfbac3e258
eoeung <eoeung113@gmail.com>
2023-12-20 16:37:14 +0900

    study ::: 클로저 추가

---
+++ src/go_basic/closure/closure.go
@@ -0,0 +1,26 @@
+package main
+
+func main() {
+	// 예제로 배우는 GO 프로그래밍
+	// [클로저]
+	// - 함수 바깥에 있는 변수를 참조하는 함수값
+	// - 함수는 바깥의 변수를 마치 함수 안으로 끌어들인 것 처럼 변수를 읽거나 쓸 수 있게 됨
+	next := nextValue() // nextValue() 함수가 리턴하는, 익명함수를 가지고 있게됨
+	println(next())     // 1 // next()는 익명함수이므로 함수처럼 호출해야함
+	println(next())     // 2
+	println(next())     // 3
+
+	anotherNext := nextValue()
+	println(anotherNext()) // 1 // 다시 시작
+	println(anotherNext()) // 2
+
+	// go by example
+}
+
+func nextValue() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
