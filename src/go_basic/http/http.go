7a65feca1eb0ef1730a908d5bbde66fc504dcb4c
eoeung <eoeung113@gmail.com>
2023-12-27 18:04:07 +0900

    docs ::: 주석 추가

--- src/go_basic/http/http.go
+++ src/go_basic/http/http.go
@@ -18,3 +18,13 @@ func (h *testHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	str := "Your Request Path is " + req.URL.Path
 	w.Write([]byte(str))
 }
+
+/*
+	＊"/" 요청을 보낼 때, ServeHTTP가 호출되는 이유
+
+		1) http.ListenAndServe(":5000", nil)
+		2) return server.ListenAndServe()
+		3) return srv.Serve(ln)
+		4) go c.serve(connCtx)
+		5) serverHandler{c.server}.ServeHTTP(w, w.req)
+*/
